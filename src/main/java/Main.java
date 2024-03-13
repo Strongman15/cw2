1be52f26a9767ef4d221da0cf617b55ade435c3c
Strongman15 <161622418+Strongman15@users.noreply.github.com>
2024-03-13 16:21:41 +0000

    ćw3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
+import java.util.Random;
 public class Main {
   public static void main(String[] args) {
-    for(int i=0; i<100;i+=4)
+  Random random = new Random();
+   int suma = 0;
+    System.out.println("Wylosowane liczby");
+    for(int i = 0; i<5;i++)
+  
       {
-          System.out.println(i);
-        }
-       
+       int liczba = random.nextInt(100);
+        System.out.println(liczba);
+        int kwadrat=liczba*liczba;
+        System.out.println(kwadrat);
+        suma+=kwadrat;
+      
+      }
+    
+ 
+    System.out.println("Suma liczb: "+suma);
+
+
+    
       }
   }
