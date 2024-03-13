6f887056e0ef32fdcd5cd95b2f08f50c3da16ade
Strongman15 <161622418+Strongman15@users.noreply.github.com>
2024-03-13 15:36:08 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+  String e = "Hello World"; 
+    for(int i=0; i<e.length();i++)
+    System.out.println(e.charAt(i));
+                
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
