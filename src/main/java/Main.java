6bfaff2d5674200f226564e1f7ff09a8c4c80b31
Strongman15 <161622418+Strongman15@users.noreply.github.com>
2024-03-13 16:06:21 +0000

    c2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-  String e = "Hello World"; 
-    for(int i=0; i<e.length();i++)
-      { for( int j = 0; j < i; j++)
-        {
-          System.out.print(" ");
+    for(int i=0; i<100;i+=4)
+      {
+          System.out.println(i);
         }
-        System.out.println(e.charAt(i));
+       
       }
   }
-}
